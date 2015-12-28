1e5fd82c63d90ec11f61ee0e306b2ecbf8002337
Cari Cesarotti <cjc359@cornell.edu>
2015-12-28 16:54:37 -0500

    runscript added

---
+++ example01Build/CMakeFiles/darkPhoton.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/darkPhoton.dir/darkPhoton.cc.o"
+  "CMakeFiles/darkPhoton.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/darkPhoton.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/darkPhoton.dir/src/TestSD.cc.o"
+  "CMakeFiles/darkPhoton.dir/src/DetectorMessenger.cc.o"
+  "CMakeFiles/darkPhoton.dir/src/RingParam.cc.o"
+  "CMakeFiles/darkPhoton.dir/src/RunAction.cc.o"
+  "CMakeFiles/darkPhoton.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/darkPhoton.dir/src/TestHit.cc.o"
+  "CMakeFiles/darkPhoton.dir/src/EventAction.cc.o"
+  "darkPhoton.pdb"
+  "darkPhoton"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/darkPhoton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
