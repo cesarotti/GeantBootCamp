1f6851fe8f761f14ecd457ed8015a30e27b81b40
Cari Cesarotti <cjc359@cornell.edu>
2016-01-11 15:12:51 -0500

    Tracker example

--- TrackingEx/OmniSD.hh
+++ TrackingEx/OmniSD.hh
@@ -5,33 +5,33 @@
  *    CJC 6.17.14 created
  */
 
-#ifndef TestSD_h
-#define TestSD_h 1
+#ifndef OmniSD_h
+#define OmniSD_h 1
 
 #include "G4VSensitiveDetector.hh"
 
-#include "TestHit.hh"
+#include "OmniHit.hh"
 
 #include <vector>
 
 class G4Step;
 class G4HCofThisEvent;
-class G4TouchableHistory;
 
-class TestSD : public G4VSensitiveDetector
+class OmniSD : public G4VSensitiveDetector
 {
 public:
-  TestSD(const G4String& name, 
+  OmniSD(const G4String& name, 
 		const G4String& hitsCollectionName);
-  virtual ~TestSD();
+  virtual ~OmniSD();
 
   //methods from base class
   virtual void Initialize(G4HCofThisEvent* hitCollection);
   virtual G4bool ProcessHits(G4Step* step, G4TouchableHistory* history);
+  virtual void EndOfEvent(G4HCofThisEvent* hitCollection);
 
 private:
-  TestHitsCollection* fHitsCollection; 
-  G4int fHCID;
+  OmniHitsCollection* fHitsCollection; 
+  G4double fOHID;
 };
 
 #endif
